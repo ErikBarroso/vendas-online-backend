26311542fd60beccfc22409cec746d3ca46bb871
Erik <barroso.erik@live.com>
2023-06-05 13:27:02 -0300

    route to fetch users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,17 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserdto } from './dto/createUserdto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Post()
-  async createUser(@Body() createUser: CreateUserdto) {
-    return createUser;
+  async createUser(@Body() createUserDto: CreateUserdto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Get()
+  async getAllUsers() {
+    return this.userService.getAllUsers();
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserdto } from './dto/createUserdto';
 import { User } from './interfaces/user.interface';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
   private users: User[] = [];
   async createUser(createUser: CreateUserdto): Promise<User> {
-    return{
-      ...createUser,
-      id: 1,
-    }
+    const saltOrRounds = 10;
+    const passwordHash = await hash(createUser.password, saltOrRounds);
 
+    //usuário do tipo da interface
+    const user: User = {
+      ...createUser,
+      id: this.users.length + 1,
+      password: passwordHash,
+    };
+    this.users.push(user);
+
+    return user;
+  }
+
+  async getAllUsers() {
+    return this.users;
   }
 }
